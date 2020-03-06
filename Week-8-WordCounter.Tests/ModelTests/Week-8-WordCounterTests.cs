bab6016bb10932b2bda6788073fe16b9f420af32
acolistro <arcolistro@gmail.com>
2020-03-06 08:15:39 -0800

    initial commit.

---
+++ Week-8-WordCounter.Tests/ModelTests/Week-8-WordCounterTests.cs
@@ -0,0 +1,18 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Counter.Models;
+using System;
+
+namespace Counter.Tests
+{
+  //change name of test class.
+  [TestClass]
+  public class ChangeNameOfClassTests
+  {
+    [TestMethod]
+    public void MethodName_WhatItDoes_ReturnObject()
+    {
+
+    }
+
+  }
+}
---
+++ Week-8-WordCounter/Models/Week-8-WordCounter.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace Counter.Models
+{
+  //change name of class.
+  class ChangeNameOfClass
+  {
+    //Change the name of the method.
+    public static void TempMethod()
+    {
+
+    }
+  }
+
+}
