566ca558c65a37c1068c4dfe4c13fc3f4ee36d0b
acolistro <arcolistro@gmail.com>
2020-03-06 12:02:18 -0800

    add scripts to program.cs to ensure that it works in the console.

--- WordCounter/Program.cs
+++ WordCounter/Program.cs
@@ -3,8 +3,19 @@ using WordCounter.Models;
 
 namespace WordCounter {
   public class Program {
-    public static void Main () {
-      Console.WriteLine ();
+    public static void Main () 
+    {
+      RepeatCounter countMe = new RepeatCounter();
+      Console.WriteLine ("please enter a sentence:");
+      string sentence = Console.ReadLine();
+      Console.WriteLine("please enter a word: ");
+      string word = Console.ReadLine();
+      if (countMe.ContainsString(sentence, word))
+      {
+        int num = countMe.HowMany(sentence, word);
+        Console.WriteLine("The sentence '" + sentence + "' contains " + num + " instances of the word '" + word + "'.");
+      }
+
     }
   }
 
