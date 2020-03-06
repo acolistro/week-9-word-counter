4c790e9c05a28ea1a7b377886d0fa07494b04322
acolistro <arcolistro@gmail.com>
2020-03-06 09:57:52 -0800

    working on resolving a type/namespace error.

---
+++ WordCounter.Tests/WordCounterTests.cs
@@ -0,0 +1,17 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WordCounter.Models;
+
+namespace WordCounter.Tests {
+  //change name of test class.
+  [TestClass]
+  public class ChangeNameOfClassTests {
+    [TestMethod]
+    public void ContainsString_ChecksStringForSubstring_bool () {
+      string sentence = "Hello world";
+      string word = "Hell";
+      Assert.isTrue (sentence, word);
+    }
+
+  }
+}
